f8f732274e6592005e458158f204f06d8e67da73
sonson <yoshida.yuichi@gmail.com>
2017-05-28 15:29:39 +0900

    Added tvOS and macOS target.

--- MiniKeychain/MiniKeychain.h
+++ MiniKeychain/MiniKeychain.h
@@ -2,12 +2,19 @@
 //  MiniKeychain.h
 //  MiniKeychain
 //
-//  Created by sonson on 2016/08/18.
-//  Copyright © 2016年 sonson. All rights reserved.
+//  Created by sonson on 2017/05/28.
+//  Copyright © 2017年 sonson. All rights reserved.
 //
 
-
 #import <Foundation/Foundation.h>
+
+#if TARGET_OS_IPHONE
+    #import <UIKit/UIKit.h>
+#elif TARGET_OS_TV
+    #import <UIKit/UIKit.h>
+#elif TARGET_OS_MAC
+    #import <Cocoa/Cocoa.h>
+#endif
 
 //! Project version number for MiniKeychain.
 FOUNDATION_EXPORT double MiniKeychainVersionNumber;
@@ -16,3 +23,5 @@ FOUNDATION_EXPORT double MiniKeychainVersionNumber;
 FOUNDATION_EXPORT const unsigned char MiniKeychainVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MiniKeychain/PublicHeader.h>
+
+
